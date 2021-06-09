60b2a79b1f71ec62a688e66fa24c42dac6b06b85
Bernando Torrez <bernandotorrez4@gmail.com>
2021-06-09 16:06:00 +0700

    first commit

---
+++ migrations/1623227453453_create-table-songs.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'varchar(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'text',
+      notNull: true,
+    },
+    year: {
+      type: 'integer',
+      notNull: true,
+    },
+    performer: {
+      type: 'text',
+      notNull: true,
+    },
+    genre: {
+      type: 'text',
+      notNull: true,
+    },
+    duration: {
+      type: 'integer',
+      notNull: true,
+    },
+    inserted_at: {
+      type: 'text',
+      notNul: true,
+    },
+    updated_at: {
+      type: 'text',
+      notNul: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
