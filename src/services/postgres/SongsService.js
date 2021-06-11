d491ff8a7548afae2c8fdc16d63fe0e03ce53ede
Bernando Torrez <bernandotorrez4@gmail.com>
2021-06-11 23:14:15 +0700

    update rekomendasi sub 1

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -14,11 +14,10 @@ class SongsService {
   }) {
     const id = uuidv4();
     const insertedAt = new Date().toISOString();
-    const updatedAt = insertedAt;
 
     const query = {
-      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
-      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $7) RETURNING id',
+      values: [id, title, year, performer, genre, duration, insertedAt],
     };
 
     const result = await this._pool.query(query);
@@ -42,7 +41,7 @@ class SongsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (result.rowCount === 0) {
       throw new NotFoundError('Lagu tidak ditemukan');
     }
 
