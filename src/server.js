839fc725de7822c850f4c65814cc0c875663f4d3
Bernando Torrez <bernandotorrez4@gmail.com>
2021-06-13 16:45:55 +0700

    updat euser

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const Hapi = require('@hapi/hapi');
+
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 require('dotenv').config();
 
 const init = async () => {
   const songsService = new SongsService();
+  const userService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -16,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: userService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
