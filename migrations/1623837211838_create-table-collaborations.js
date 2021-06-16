85c1e22480b53e83e01679dade9ceaaefb5db9af
Bernando Torrez <bernandotorrez4@gmail.com>
2021-06-16 17:22:33 +0700

    update

--- migrations/1623837211838_create-table-collaborations.js
+++ migrations/1623837211838_create-table-collaborations.js
@@ -1,7 +1,20 @@
-/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'varchar(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'varchar(50)',
+      notNull: true,
+    },
+  });
+};
 
-exports.shorthands = undefined;
-
-exports.up = pgm => {};
-
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
